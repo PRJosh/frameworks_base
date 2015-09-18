2b251320e8b39697ede2bcff1320343d6a3c7e29
Yashdev Singh <yashdevs@codeaurora.org>
2015-09-18 10:32:04 -0700

    Enable default APN activation on non-dds for DSDA.
    - Add new Setting DB entry to enable this functionality.
    - Default value of the feature is controlled by resource config which
      could be changed with overlay as per oem customizations.
    
    Change-Id: Ia6e60bf03c325ec525f8ca39cbacc2655675a829

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4453,6 +4453,7 @@ public final class Settings {
             MOVED_TO_GLOBAL.add(Settings.Global.DEFAULT_DNS_SERVER);
             MOVED_TO_GLOBAL.add(Settings.Global.PREFERRED_NETWORK_MODE);
             MOVED_TO_GLOBAL.add(Settings.Global.WEBVIEW_DATA_REDUCTION_PROXY_KEY);
+            MOVED_TO_GLOBAL.add(Settings.Global.VSIM_FEATURE);
         }
 
         /**
@@ -7044,6 +7045,9 @@ public final class Settings {
         * @hide
         */
        public static final String MOBILE_DATA = "mobile_data";
+
+       /** {@hide} */
+       public static final String VSIM_FEATURE = "vsim_feature";
 
        /** {@hide} */
        public static final String NETSTATS_ENABLED = "netstats_enabled";
